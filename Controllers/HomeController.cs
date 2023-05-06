e3e5d33605e39a6bac89a0aa4ddcb8133cc2c746
hadil23 <hadil.afi@isimg.tn>
2023-05-06 20:35:58 +0100

    final

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -37,6 +37,17 @@ namespace BookStore.Controllers
             return View();
         }
 
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
